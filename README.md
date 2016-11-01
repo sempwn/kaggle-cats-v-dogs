daa360c28f220d5d6703be9b2f09a1496f7b13fb
Mike Irvine <michael.irvine.mai@googlemail.com>
2016-10-31 21:03:21 -0700

    first working version with model prediction.

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 ![cats-v-dogs](https://kaggle2.blob.core.windows.net/competitions/kaggle/3362/media/woof_meow.jpg)
 ---
 Kaggle competition entry for [Dogs vs. Cats redux](https://github.com/sempwn/kaggle-cats-v-dogs.git)
+
+---
+
+## Setup
+
+Uses python packages `keras` with a `theano` back-end. I implemented this in `Python 2.7`.
+Model weights used from Keras model of the 16-layer network used by the VGG team in the ILSVRC-2014 competition. Download can be found here: [https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3](link).
+Just make sure it's downloaded in the appropriate sub-directory.
+Also uses `Tkinter` to open file image for prediction.
+---
+## Competition notes and resources
+* Good blog post from keras covering how to train a model using a small amount of data (examples are from this competition)[https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html](link).
+* @bigsnarfdude's blog post on setting up keras for the competition: [https://bigsnarf.wordpress.com/2016/10/22/keras-cats-and-dogs/](link).
+* @Gauss256's notes on the competition: [https://github.com/gauss256/dogs-vs-cats](link).
---
+++ predict_image.py
@@ -0,0 +1,145 @@
+'''This script goes along the blog post
+"Building powerful image classification models using very little data"
+from blog.keras.io.
+It uses data that can be downloaded at:
+https://www.kaggle.com/c/dogs-vs-cats/data
+In our setup, we:
+- created a data/ folder
+- created train/ and validation/ subfolders inside data/
+- created cats/ and dogs/ subfolders inside train/ and validation/
+- put the cat pictures index 0-999 in data/train/cats
+- put the cat pictures index 1000-1400 in data/validation/cats
+- put the dogs pictures index 12500-13499 in data/train/dogs
+- put the dog pictures index 13500-13900 in data/validation/dogs
+So that we have 1000 training examples for each class, and 400 validation examples for each class.
+In summary, this is our directory structure:
+```
+data/
+    train/
+        dogs/
+            dog001.jpg
+            dog002.jpg
+            ...
+        cats/
+            cat001.jpg
+            cat002.jpg
+            ...
+    validation/
+        dogs/
+            dog001.jpg
+            dog002.jpg
+            ...
+        cats/
+            cat001.jpg
+            cat002.jpg
+            ...
+```
+'''
+
+import os
+import h5py
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image as image_utils
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+
+#image input utils
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
+
+# path to the model weights files.
+weights_path = 'data/models/vgg16_weights.h5'
+top_model_weights_path = 'data/models/bottleneck_fc_model.h5'
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+nb_train_samples = 2000
+nb_validation_samples = 800
+nb_epoch = 50
+
+# build the VGG16 network
+model = Sequential()
+model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))
+
+model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+# load the weights of the VGG16 networks
+# (trained on ImageNet, won the ILSVRC competition in 2014)
+# note: when there is a complete match between your model definition
+# and your weight savefile, you can simply call model.load_weights(filename)
+assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
+f = h5py.File(weights_path)
+for k in range(f.attrs['nb_layers']):
+    if k >= len(model.layers):
+        # we don't look at the last (fully-connected) layers in the savefile
+        break
+    g = f['layer_{}'.format(k)]
+    weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
+    model.layers[k].set_weights(weights)
+f.close()
+print('Model loaded.')
+
+# build a classifier model to put on top of the convolutional model
+top_model = Sequential()
+top_model.add(Flatten(input_shape=model.output_shape[1:]))
+top_model.add(Dense(256, activation='relu'))
+top_model.add(Dropout(0.0)) #Should have 0 dropout for predicition. But still need model structure so set to 0.
+top_model.add(Dense(1, activation='sigmoid'))
+
+print('[INFO] loading weights. May take a while...')
+# note that it is necessary to start with a fully-trained
+# classifier, including the top classifier,
+# in order to successfully do fine-tuning
+top_model.load_weights(top_model_weights_path)
+
+# add the model on top of the convolutional base
+model.add(top_model)
+# TODO: create test_data in appropriate format.
+
+print("[INFO] loading and preprocessing image...")
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+image = image_utils.load_img(filename, target_size=(img_width, img_height))
+image = image_utils.img_to_array(image) #array should be (3,150,150)
+image = np.expand_dims(image, axis=0) #expand to shape (1,3,150, 150)
+pDOG = model.predict(image)[0][0]
+pCAT = 1. - pDOG
+print 'Image {} percent dog and {} percent cat'.format(pDOG*100.,pCAT*100.)
